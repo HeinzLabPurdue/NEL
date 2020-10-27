3d94317b4dec0b6321ebbf9d2f3c614aed749130
Heinz Lab Neurophysiology User Account <Heinz Lab - Neurophy@HHSSLHS-FKJ1JL2.BoilerAD.Purdue.edu>
2020-10-27 19:54:18 -0400

    R.S comitting changes in workspace

---
+++ Users/SP/SP_nel_gui/locateCurrentStimuli.m
@@ -0,0 +1,19 @@
+%% Locate current stimuli to be saved in to text file
+%HG - 10/27/20
+
+%This function is called to save the currentFreq and currentLevel of
+%certain trigger
+function [currentFreq, currentLevel] = locateCurrentStimuli(x)
+
+
+
+
+
+
+
+
+
+
+
+
+end
