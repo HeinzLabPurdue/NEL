843e77587fb0f37d6cae2f4e54b4638dbe9ac5ab
Heinz Lab - NEL2 <Heinz Lab - NEL2@HHSSLHS-FKJ1JL2.BoilerAD.Purdue.edu>
2023-06-27 15:22:43 -0400

    confirmed ABR calib fixed set up in Master to merge with AEP changes

--- Nel_matlab/AEP/ABR/make_ABR_text_file_subfunc2.m
+++ Nel_matlab/AEP/ABR/make_ABR_text_file_subfunc2.m
@@ -1,12 +1,6 @@
 % make_ABR_text_file_subfunc2.m
 
-rc = write_nel_data(fname,x,0);   
 save(fname,'x') %save mat-file along side picture file JMR Sept 21
-while (rc < 0)
-	title_str = ['Choose a different file name! Can''t write to ''' fname ''''];
-	[fname dirname] = uiputfile([fileparts(fname) filesep '*.m'],title_str);
-	rc = write_nel_data(fullfile(dirname,fname),x,0);
-end
 
 % aux_x=x;
 % aux_x.AD_Data=rmfield(aux_x.AD_Data,'AD_All_V');
