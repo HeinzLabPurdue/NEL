79cdc65c05c6644edcba470a5463baf8bbebcaef
hausersn1 <samantha.n.hauser@gmail.com>
2023-06-29 14:16:55 -0500

    FPL PB icon | SH

--- Nel_matlab/nel_general/startup.m
+++ Nel_matlab/nel_general/startup.m
@@ -29,6 +29,7 @@ addpath([root_dir  'DPOAE']);
 addpath([root_dir  'AEP']);
 addpath([root_dir  'WBMEMR']);
 addpath([root_dir 'AdvOAE']); 
+addpath([root_dir 'FPL']); 
 addpath(genpath([root_dir  'FFR']));
 addpath([root_dir  'NELanalysis_Mfiles']);
 % addpath([root_dir  'wiring_test']);   %LQ 10/29/04
