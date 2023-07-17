516a5e54b2a4cb72ec57e51295c1daaac0053020
Heinz Lab - NEL1 <Heinz Lab - NEL1@HHSSLHS-FKJ2JL2.BoilerAD.Purdue.edu>
2023-07-17 12:30:00 -0400

    fixed startup file

--- Nel_matlab/nel_general/startup.m
+++ Nel_matlab/nel_general/startup.m
@@ -28,13 +28,10 @@ addpath([root_dir  'DPOAE']);
 % addpath([root_dir  'CAP']);
 addpath([root_dir  'AEP']);
 addpath([root_dir  'WBMEMR']);
-<<<<<<< HEAD
 addpath([root_dir 'AdvOAE']); 
 addpath([root_dir 'FPL']); 
 addpath(genpath([root_dir  'FFR']));
-=======
 % addpath(genpath([root_dir  'FFR']));
->>>>>>> master
 addpath([root_dir  'NELanalysis_Mfiles']);
 % addpath([root_dir  'wiring_test']);   %LQ 10/29/04
 
