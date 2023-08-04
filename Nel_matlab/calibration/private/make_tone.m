6a05c81bac2863b25fa7806647f397dd079aeac8
Heinz Lab - NEL2 <Heinz Lab - NEL2@HHSSLHS-FKJ1JL2.BoilerAD.Purdue.edu>
2023-08-04 18:49:05 -0400

    AS/MH | Minor changes and calibration/OAE comments after debugging mixer selector

--- Nel_matlab/calibration/private/make_tone.m
+++ Nel_matlab/calibration/private/make_tone.m
@@ -58,29 +58,33 @@ if Stimuli.ear == 1 %left ear
     %     status1 = invoke(COMM.handle.RP2_1, 'ConnectRP2', NelData.General.TDTcommMode, 1);
     [COMM.handle.RP2_1, status1]=connect_tdt('RP2', 1);
     invoke(COMM.handle.RP2_1,'LoadCof',[object_dir '\make_tone_left.rcx']);
-    invoke(COMM.handle.RP2_1,'SetTagVal','Select',160);
+    invoke(COMM.handle.RP2_1,'SetTagVal','Select',160); % original
+    % 8/4/23 (MH/AS): Allow pre-PA1/2 cross over (Select) to avoid LE distortion/-30dB atten from RP2-2Out1.  Use post PA1/2 cross-over (Connect) to aviod the bad hardware on HeinzLab NEL2 Mix/select
+%     invoke(COMM.handle.RP2_1,'SetTagVal','Select',120); % Hack to aviod bad mix/selector hardware on LE 
     invoke(COMM.handle.RP2_1,'Run');
     
     %     COMM.handle.RP2_2 = actxcontrol('RPco.x',[0 0 5 5]);
     %     status2 = invoke(COMM.handle.RP2_2, 'ConnectRP2', NelData.General.TDTcommMode, 2);
     [COMM.handle.RP2_2, status2 ]=connect_tdt('RP2', 2);
     invoke(COMM.handle.RP2_2,'LoadCof',[object_dir '\make_tone_right_PU.rcx']);
-    invoke(COMM.handle.RP2_2,'SetTagVal','Select', 56);
+     invoke(COMM.handle.RP2_2,'SetTagVal','Select', 56);   % original
+    % 8/4/23 (MH/AS): Allow pre-PA1/2 cross over (Select) to avoid LE distortion/-30dB atten from RP2-2Out1.  Use post PA1/2 cross-over (Connect) to aviod the bad hardware on HeinzLab NEL2 Mix/select
+%     invoke(COMM.handle.RP2_2,'SetTagVal','Select', 0);
     invoke(COMM.handle.RP2_2,'Run');
-else
+else   % right ear
     
     %     COMM.handle.RP2_1 = actxcontrol('RPco.x',[0 0 5 5]);
     %     status1 = invoke(COMM.handle.RP2_1, 'ConnectRP2', NelData.General.TDTcommMode, 1);
     [COMM.handle.RP2_1, status1]=connect_tdt('RP2', 1);
     invoke(COMM.handle.RP2_1,'LoadCof',[object_dir '\make_tone_left.rcx']);
-    invoke(COMM.handle.RP2_1,'SetTagVal','Select',56);
+    invoke(COMM.handle.RP2_1,'SetTagVal','Select',56); 
     invoke(COMM.handle.RP2_1,'Run');
     
     %     COMM.handle.RP2_2 = actxcontrol('RPco.x',[0 0 5 5]);
     %     status2 = invoke(COMM.handle.RP2_2, 'ConnectRP2', NelData.General.TDTcommMode, 2);
     [COMM.handle.RP2_2, status2]=connect_tdt('RP2', 2);
     invoke(COMM.handle.RP2_2,'LoadCof',[object_dir '\make_tone_right_PU.rcx']);
-    invoke(COMM.handle.RP2_2,'SetTagVal','Select',64);
+    invoke(COMM.handle.RP2_2,'SetTagVal','Select',64);   
     invoke(COMM.handle.RP2_2,'Run');
 end
 if ~status1 || ~status2
